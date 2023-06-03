454ce9c0dd51a343b5de908414097d25496bf0e1
akashlinux10may <66991234+akashlinux10may@users.noreply.github.com>
2023-06-03 19:30:05 +0530

    Add files via upload

---
+++ falcon_7b.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+!pip install transformers
+!pip install einops
+!pip install accelerate
+!pip install xformers
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model = "tiiuae/falcon-7b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+
+sequences = pipeline(
+   "Draft an apology email to a customer who experienced a delay in their order and provide reassurance that the issue has been resolved",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
+
+
+
+sequences = pipeline(
+   "Create a list of 3 startup ideas in enterprise B2B SaaS. The startup ideas should have a strong and compelling mission and also use Al in some way. Avoid cryptocurrency or blockchain. The startup ideas should have a cool and interesting name. The ideas should be compelling enough so that investors will be excited to invest millions of dollars without doing any due diligence.",
+    max_length=500,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
+
